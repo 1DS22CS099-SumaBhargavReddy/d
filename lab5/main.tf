61d95c3829f724aafaf3755a09e54adaa9710931
Kaipa Suma Bhargav Reddy <137994264+1DS22CS099-SumaBhargavReddy@users.noreply.github.com>
2025-06-29 21:00:02 +0530

    Update main.tf

--- lab5/main.tf
+++ lab5/main.tf
@@ -26,3 +26,24 @@ resource "aws_s3_bucket_object" "text_file" {
   source = "C:\\Users\\nithi\\OneDrive\\Desktop\\Sample.txt"
 
 }
+
+Step 3: Initialize Terraform: execute following command on VS code terminal :
+
+terraform init
+
+This sets up Terraform in the folder and downloads necessary provider plugins.
+Step 4: Preview the Plan
+
+terraform plan
+
+This shows what Terraform will create.
+Step 5: Apply the Configuration
+
+terraform apply
+
+When prompted, type yes to confirm. Terraform will create the EC2 instance.
+Step 6: Verify
+Go to your AWS Console → EC2 → Instances, and you’ll see the instance running.
+Step 7: Destroy When Done (to avoid charges)
+
+terraform destroy
